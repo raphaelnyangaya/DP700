1003c8c7d1c009202539d7fd2448964e661c0212
raphael <95930804+raphaelnyangaya@users.noreply.github.com>
2025-02-13 09:14:30 +0300

    Committing 1 item from workspace 2eeb9d42-3d32-4c54-a6ce-0cf5107eb2de

---
+++ LoadTest1.Notebook/notebook-content.py
@@ -0,0 +1,22 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+
+#Initial Load
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
